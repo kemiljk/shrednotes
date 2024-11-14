00bae49c29ddcea62e7a78eb34aa7d61425c396c
Karl Koch <karl@kejk.tech>
2024-11-14 10:52:04 +0000

    Updates to include new widgets and a series of improvements to get things back to good

---
+++ Widget/WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Karl Koch on 14/11/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Widget()
+    }
+}
